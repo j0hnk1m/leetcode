cd163c8a95a76dae56b1b18ea75ffec05c0fd492
j0hnk1m <kim.john228@gmail.com>
2019-08-31 19:58:32 -0700

    updated structure + finished 3

---
+++ 3.py
@@ -0,0 +1,17 @@
+s='abcabcbb'
+used = {}
+window_start = 0
+window_end = 0
+ans = 0
+
+while window_start != len(s) and window_end != len(s):
+    if s[window_end] not in used:
+        used[s[window_end]] = window_end
+        window_end += 1
+        if window_end - window_start > ans:
+            ans = window_end - window_start
+    else:
+        del used[s[window_start]]
+        window_start += 1 
+
+return ans
