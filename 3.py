93c803e0d1cfcd779f12555840a5e278385e6e2a
j0hnk1m <kim.john228@gmail.com>
2019-08-31 19:59:38 -0700

    fixed 3

--- 3.py
+++ 3.py
@@ -1,17 +1,18 @@
-s='abcabcbb'
-used = {}
-window_start = 0
-window_end = 0
-ans = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        used = {}
+        window_start = 0
+        window_end = 0
+        ans = 0
 
-while window_start != len(s) and window_end != len(s):
-    if s[window_end] not in used:
-        used[s[window_end]] = window_end
-        window_end += 1
-        if window_end - window_start > ans:
-            ans = window_end - window_start
-    else:
-        del used[s[window_start]]
-        window_start += 1 
+        while window_start != len(s) and window_end != len(s):
+            if s[window_end] not in used:
+                used[s[window_end]] = window_end
+                window_end += 1
+                if window_end - window_start > ans:
+                    ans = window_end - window_start
+            else:
+                del used[s[window_start]]
+                window_start += 1 
 
-return ans
+        return ans
