f49178616d7d45d9af377b49aa4c732e94fcb44a
j0hnk1m <kim.john228@gmail.com>
2019-09-03 22:12:25 -0700

    added new solution for 189

--- easy/189.py
+++ easy/189.py
@@ -9,20 +9,26 @@ class Solution:
 			new[(i + k) % len(nums)] = nums[i]
 		nums[:] = new
 
-		# # 2. in-place by storing previous value
+		# # 2. in-place cyclic replacement
 		# n_assignments = 0
+		# used_idx = {}
 		# idx = 0
 		# tmp1, tmp2 = None, None
 		# while n_assignments < len(nums):
-		# 	if n_assignments == 0:
+		# 	if idx in used_idx:
+		# 		idx += 1
 		# 		tmp1 = nums[idx]
 		# 	else:
-		# 		tmp1 = tmp2
+		# 		used_idx[idx] = 1
 		#
-		# 	tmp2 = nums[(idx + k) % len(nums)]
-		# 	nums[(idx + k) % len(nums)] = tmp1
-		# 	idx = (idx+k)%len(nums)
-		# 	n_assignments += 1
+		# 		if n_assignments == 0:
+		# 			tmp1 = nums[idx]
+		#
+		# 		tmp2 = nums[(idx + k) % len(nums)]
+		# 		nums[(idx + k) % len(nums)] = tmp1
+		# 		tmp1 = tmp2
+		# 		idx = (idx+k)%len(nums)
+		# 		n_assignments += 1
 
 
 		# # 3. reversing parts of array
