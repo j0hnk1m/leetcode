d9831be6647592e961059201780307f80845aaf1
j0hnk1m <kim.john228@gmail.com>
2019-09-04 11:07:55 -0700

    cleaned 118

--- easy/118.py
+++ easy/118.py
@@ -3,16 +3,14 @@ class Solution:
         if numRows == 0:
             return
 
-        pascal = [[0, 1, 0]]
+        pascal = [[1]]
         for i in range(numRows - 1):
             prevrow = pascal[-1]
-            newrow = [0]
+            newrow = [1]
 
             for h in range(len(prevrow) - 1):
                 newrow.append(prevrow[h + 1] + prevrow[h])
-            newrow.append(0)
-            pascal[-1][:] = pascal[-1][1:-1]
+            newrow.append(1)
             pascal.append(newrow)
 
-        pascal[-1][:] = pascal[-1][1:-1]
         return pascal
