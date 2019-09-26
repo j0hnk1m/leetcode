4f888909e811592ca02bc006f060f2aba107de3b
j0hnk1m <kim.john228@gmail.com>
2019-09-26 11:56:56 -0700

    did 389

---
+++ easy/389.py
@@ -0,0 +1,20 @@
+class Solution:
+	def findTheDifference(self, s: str, t: str) -> str:
+		hashmap = {}
+		for i in s:
+			if i in hashmap:
+				hashmap[i] += 1
+			else:
+				hashmap[i] = 1
+
+		for i in t:
+			if i in hashmap:
+				hashmap[i] -= 1
+			else:
+				hashmap[i] = 1
+
+		unique = ''
+		for k in hashmap.keys():
+			if hashmap[k] != 0:
+				unique = k
+		return unique
