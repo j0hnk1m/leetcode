d67a2027b081cb5d0d5d03842aeb491b52a8d90c
j0hnk1m <kim.john228@gmail.com>
2019-09-30 23:11:28 -0700

    did 67

---
+++ easy/67.py
@@ -0,0 +1,22 @@
+class Solution:
+	def addBinary(self, a: str, b: str) -> str:
+		carry = 0
+		binarysum = ''
+
+		while a or b:
+			if not a:
+				a = '0'
+			elif not b:
+				b = '0'
+
+			ab = int(a[-1]) + int(b[-1]) + carry
+			carry = ab // 2
+			ab %= 2
+			binarysum = ''.join([str(ab), binarysum])
+			a = a[:-1]
+			b = b[:-1]
+
+		if carry:
+			binarysum = ''.join([str(carry), binarysum])
+
+		return binarysum
