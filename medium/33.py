d5fa67cc411dda4be2214761b8b42bcf9a611954
j0hnk1m <kim.john228@gmail.com>
2019-12-26 11:35:33 -0800

    did 33

--- medium/33.py
+++ medium/33.py
@@ -1,3 +1,31 @@
 nums = [4,5,6,7,0,1,2]
 target = 0
 
+if not nums:
+    return -1
+
+l = 0
+r = len(nums) - 1
+while l < r:
+    mid = l + (r-l)//2
+    if nums[mid] > nums[r]:
+        l = mid + 1
+    else:
+        r = mid
+
+start = l
+l = 0
+r = len(nums) - 1
+if target >= nums[start] and target <= nums[r]:
+    l = start
+else:
+    r = start
+while l <= r:
+    mid = l + (r-l)//2
+    if target == nums[mid]:
+        return mid
+    elif target > nums[mid]:
+        l = mid + 1
+    else:
+        r = mid - 1
+return -1
