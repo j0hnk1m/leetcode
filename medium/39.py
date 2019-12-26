68128ee89ca3ef6438f2cfbb6322adeff2c36e57
j0hnk1m <kim.john228@gmail.com>
2019-12-26 13:12:12 -0800

    fixed 39

--- medium/39.py
+++ medium/39.py
@@ -1,15 +1,15 @@
 candidates = [2,3,6,7]
 target = 7
 
-def dfs(path, candidates, target, res):
+def dfs(path, nums, target, res):
     if target < 0:
         return
     elif target == 0:
         res.append(path)
         return 
     
-    for i in range(len(candidates)):
-        dfs(path+[candidates[i]], candidates[i:], target-candidates[i], res)
+    for i in range(len(nums)):
+        dfs(path+[nums[i]], nums[i:], target-nums[i], res)
 
 res = []
 candidates.sort()
