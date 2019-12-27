13b97ec5a4c87dfa79be797a1e226bc42c822619
j0hnk1m <kim.john228@gmail.com>
2019-12-26 18:16:14 -0800

    fixed 70

--- easy/70.py
+++ easy/70.py
@@ -1,7 +1,7 @@
 n = 3
 
-steps = [1,1]
+dp = [1,1]
 if n >= 2:
     for i in range(2, n+1):
-        steps.append(steps[i-1] + steps[i-2])
-return steps[-1]
+        dp.append(dp[i-1] + dp[i-2])
+return dp[n]
