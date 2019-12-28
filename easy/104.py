a9fdaf01720722bc8b54d6bc0aaa85a26cc2fec3
j0hnk1m <kim.john228@gmail.com>
2019-12-27 16:42:26 -0800

    added iterative bfs solution

--- easy/104.py
+++ easy/104.py
@@ -9,7 +9,24 @@ root.right = TreeNode(20)
 root.right.left = TreeNode(15)
 root.right.right = TreeNode(7)
 
-
+# recursive dfs - o(n) runtime
 if not root:
     return 0
 return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+# iterative bfs - o(n) runtime, o(n) space
+if not root:
+    return 0
+
+q = [root]
+depth = 0
+while q:
+    depth += 1
+    next_level = []
+    for i in q:
+        if i.left:
+            next_level.append(i.left)
+        if i.right:
+            next_level.append(i.right)
+    q = next_level
+return depth
--- hard/295.py
+++ hard/295.py
@@ -1,5 +1,6 @@
 from heapq import *
 
+# using two heaps
 class MedianFinder:
     def __init__(self):
         """
@@ -8,13 +9,13 @@ class MedianFinder:
         self.small = []  # maxheap
         self.large = []  # minheap
         
-    def addNum(self, num: int) -> None:
+    def addNum(self, num: int) -> None:  # o(logn) runtime, o(n) space
         if len(self.small) == len(self.large):
             heappush(self.large, -heappushpop(self.small, -num))
         else:
             heappush(self.small, -heappushpop(self.large, num))
 
-    def findMedian(self) -> float:
+    def findMedian(self) -> float:  # o(1) runtime, o(1) space
         if len(self.small) == len(self.large):
             return float(-self.small[0] + self.large[0]) / 2
         return float(self.large[0])
