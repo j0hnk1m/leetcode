d8a511c56920a9bd15328630b3b3f3ec4e107083
j0hnk1m <kim.john228@gmail.com>
2019-12-27 23:21:01 -0800

    done

--- easy/572.py
+++ easy/572.py
@@ -8,9 +8,21 @@ s.left = TreeNode(4)
 s.right = TreeNode(5)
 s.left.left = TreeNode(1)
 s.left.right = TreeNode(2)
-t = TreeNode(5)
+t = TreeNode(4)
 t.left = TreeNode(1)
 t.right = TreeNode(2)
 
-# iterative bfs
+# recursive
+def equals(a, b):
+    if not a and not b:
+        return True
+    elif not a or not b:
+        return False
+    return a.val == b.val and equals(a.left, b.left) and equals(a.right, b.right)
 
+def isSubtree(s: TreeNode, t: TreeNode) -> bool:
+    if not s and not t:
+        return True
+    elif not s or not t:
+        return False
+    return equals(s, t) or isSubtree(s.left, t) or isSubtree(s.right, t)
