fa074f38de2049bb8b47410921327d2779c365c6
j0hnk1m <kim.john228@gmail.com>
2019-12-29 12:17:41 -0800

    candice

--- medium/5.py
+++ medium/5.py
@@ -1,17 +1,11 @@
 s = 'babbad'
 
 def expand(s, left, right):
-    if not s:
-        return 0
-
     while left >= 0 and right < len(s) and s[left] == s[right]:
         left -= 1
         right += 1
     return right - left - 1
     
-
-if not s:
-    return ''
 
 start = end = 0
 for i in range(len(s)):
