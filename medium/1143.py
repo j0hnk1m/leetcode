12856a5bfad2ff550d86000a061280fd4f70edae
j0hnk1m <kim.john228@gmail.com>
2019-12-29 16:29:45 -0800

    i hate dp

--- medium/1143.py
+++ medium/1143.py
@@ -1,3 +1,30 @@
 text1 = "abcde"
 text2 = "ace" 
 
+# recursive - o(2^(m+n)) runtime, o(m+n) space
+def longestCommonSubsequence(text1, text2):
+    if not text1 or not text2:
+        return 0
+    elif text1[0] == text2[0]:
+        return 1 + longestCommonSubsequence(text1[1:], text2[1:])
+    return max(longestCommonSubsequence(text1, text2[1:]), longestCommonSubsequence(text1[1:], text2))
+
+# recursive w/ memoization - o(mn) runtine, o(mn) space
+def longestCommonSubsequence(text1, text2):
+    memo = {}
+    lcs(text1, text2)
+    return memo[(text1, text2)]
+
+def lcs(t1, t2):
+    if not t1 or not t2:
+        res = 0
+    elif (t1, t2) in memo:
+        res = memo[(t1, t2)]
+    elif t1[0] == t2[0]:
+        res = 1 + lcs(t1[1:], t2[1:])
+    else:
+        res = max(lcs(t1[1:], t2), lcs(t1, t2[1:]))
+    
+    memo[(t1, t2)] = res
+    return res
+
--- medium/300.py
+++ medium/300.py
@@ -5,13 +5,9 @@ if not nums:
 
 dp = [0 for i in range(len(nums))]
 dp[0] = 1
-lis = 1
 for i in range(len(nums)):
-    max_val = 0
     for j in range(i):
         if nums[j] < nums[i]:
-            max_val = max(max_val, dp[j])
-    dp[i] = max_val + 1
-    lis = max(lis, dp[i])
-
-return lis 
+            dp[i] = max(dp[i], dp[j]+1)
+    dp[i] = max(1, dp[i])
+return max(dp)
